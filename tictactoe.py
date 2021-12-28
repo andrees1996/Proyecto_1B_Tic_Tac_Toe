179f724585fbe45fbed11d1a3fec27ef42bc594a
Andres Vargas <stalin.vargas@epn.edu.ec>
2021-12-27 23:05:52 -0500

    Update/utility

--- tictactoe.py
+++ tictactoe.py
@@ -2,7 +2,7 @@
 Tic Tac Toe Player
 """
 
-import math
+
 
 from Proyecto_1B_Tic_Tac_Toe.runner import board
 import math
@@ -52,33 +52,82 @@ def actions(board):
                 conjunto_movimientos.append([fila, columna])
 
     return conjunto_movimientos
-    
+
 def result(board, action):
-    """
-    Returns the board that results from making move (i, j) on the board.
-    """
-    raise NotImplementedError
+    """se realiza la copia del trablero"""
+    boardcopy = copy.deepcopy(board)
+
+    if boardcopy[action[0]][action[1]] != EMPTY:
+            raise IndexError
+    else:
+        boardcopy[action[0]][action[1]] = player(boardcopy)
+        return boardcopy
+
 
 
 def winner(board):
-    """
-    Returns the winner of the game, if there is one.
-    """
-    raise NotImplementedError
+    columns = []
+    """aqui se verifica si es que existio un ganador , a traves de las filas de la tabla"""
+    for fila in board:
+        numero_piezas_X = fila.count(X)
+        numero_piezas_O = fila.count(O)
+        if numero_piezas_X == 3:
+            return X
+        if numero_piezas_O == 3:
+            return O
+
+    """aqui se verifica si es que existio un ganador , a traves de las columnas de la tabla"""
+    for columna in range(len(board)):
+        column = [fila[columna] for fila in board]
+        columns.append(column)
+
+    for j in columns:
+        numero_piezas_X = columna.count(X)
+        numero_piezas_O = columna.count(O)
+        if numero_piezas_X == 3:
+            return X
+        if numero_piezas_O == 3:
+            return O
+
+    """se verifica si es que existio un ganador a trves de las diagonales de la tabla"""
+    if board[1][1] == O:
+        if (board[0][0] == 0 and board[2][2] == 0) or (board[0][2] == 0 and board[2][0] == 0):
+            return 0
+
+    if board[1][1] == X:
+        if (board[0][0] == X and board[2][2] == X) or (board[0][2] == X and board[2][0] == X):
+            return X
+
+    """Si existe empate"""
+    return None
 
 
 def terminal(board):
-    """
-    Returns True if game is over, False otherwise.
-    """
-    raise NotImplementedError
+    """verifica si el juego a terminado"""
+    contador = 0
+    for fila in board:
+        """cuenta las celdas que estan vacias"""
+        contador = contador + fila.count(EMPTY)
+        """si el contador tiene como valor final 0 , quiere decir que se completaron todas las celdas"""
+    if contador == 0:
+        return True
+        """se llama al metodo winner para determinar si existio un ganador"""
+    elif winner(board) is not None:
+        return True
+        """si no paso ninguna de las dos opciones, quiere decir que aun existen casillas vacias"""
+    else:
+        return False
 
 
 def utility(board):
-    """
-    Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
-    """
-    raise NotImplementedError
+    """si el ganador es X retorna 1, si el ganador es o retorna -1 y si existe un empate retorna 0"""
+    if winner(board) == X:
+        return 1
+    elif winner(board) == O:
+        return -1
+    else:
+        return 0
+
 
 
 def minimax(board):
