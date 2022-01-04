9bd48b061e0fcbb5faf996d67fb06e36cee6bac5
Andres Vargas <stalin.vargas@epn.edu.ec>
2022-01-03 22:27:05 -0500

    Update/Final

--- tictactoe.py
+++ tictactoe.py
@@ -7,11 +7,14 @@ import copy
 X = "X"
 O = "O"
 EMPTY = None
+global movimientos_posibles
+movimientos_posibles: int = 0
 
 
 def initial_state():
     """
-    Returns starting state of the board.
+    se muestra el estado inicial del tablero ,
+    en este caso todas las celdas estan vacias
     """
     return [[EMPTY, EMPTY, EMPTY],
             [EMPTY, EMPTY, EMPTY],
@@ -19,15 +22,17 @@ def initial_state():
 
 
 def player(board):
-
-    """si es el primer movimiento entonces , se establece x como primer movimiento y en este caso se compara con la funcion initial_state()"""
+    """si es el primer movimiento entonces ,
+    se establece x como primer movimiento
+    y en este caso se compara con la funcion initial_state()"""
     numero_piezas_X = 0
     numero_piezas_O = 0
     if board == initial_state():
         return X
 
     for fila in board:
-        """por cada fila se contra el numero de X e O que se encuentran en el tablero para de esta manera poder determinar el movmiento"""
+        """por cada fila se contra el numero de X e O que se encuentran 
+        en el tablero para de esta manera poder determinar el movmiento"""
         numero_piezas_X = numero_piezas_X + fila.count(X)
         numero_piezas_O = numero_piezas_O + fila.count(O)
 
@@ -36,22 +41,26 @@ def player(board):
     elif numero_piezas_O == numero_piezas_X:
         return X
 
+
 def actions(board):
-    """Se debe devolver n conjunto de todas las acciones posibles que se pueden tomar en el tablero"""
+    """Se debe devolver n conjunto de todas las acciones posibles
+     que se pueden tomar en el tablero"""
     conjunto_movimientos = []
-    """como se debe devolver una tupla, se tiene que realizar una matri bidimensional y recorrer en ella """
+    """como se debe devolver una tupla, se tiene que 
+    realizar una matriz bidimensional y recorrer en ella """
     for fila in range(3):
         for columna in range(3):
             if board[fila][columna] == EMPTY:
                 conjunto_movimientos.append([fila, columna])
     return conjunto_movimientos
 
+
 def result(board, action):
     """se realiza la copia del trablero"""
     copia_board = copy.deepcopy(board)
 
     if copia_board[action[0]][action[1]] != EMPTY:
-            raise IndexError
+        raise IndexError
     else:
         copia_board[action[0]][action[1]] = player(copia_board)
         return copia_board
@@ -67,8 +76,7 @@ def winner(board):
             return X
         if numero_piezas_O == 3:
             return O
-
-    """aqui se verifica si es que existio un ganador , a traves de las columnas de la tabla"""
+    """aqui se verifica si es que existio un ganador ,  a traves de las columnas de la tabla"""
     for columna in range(len(board)):
         column = [fila[columna] for fila in board]
         columns.append(column)
@@ -81,15 +89,14 @@ def winner(board):
         if numero_piezas_O == 3:
             return O
 
-    """se verifica si es que existio un ganador a trves de las diagonales de la tabla"""
-
     """Se verifica si existio el ganador O a traves de las diagonales"""
-    if (board[1][1] == O and board[0][0] == O and board[2][2] == O) or (board[1][1] == O and board[0][2] == O and board[2][0] == O):
+    if (board[1][1] == O and board[0][0] == O and board[2][2] == O) or (
+            board[1][1] == O and board[0][2] == O and board[2][0] == O):
         return 0
     """Se verifica si existio el ganador X a traves de las diagonales"""
-    if (board[1][1] == X and board[0][0] == X and board[2][2] == X) or (board[1][1] == X and board[0][2] == X and board[2][0] == X):
-            return X
-
+    if (board[1][1] == X and board[0][0] == X and board[2][2] == X) or (
+            board[1][1] == X and board[0][2] == X and board[2][0] == X):
+        return X
     """Si existe empate"""
     return None
 
@@ -103,6 +110,7 @@ def terminal(board):
         """si el contador tiene como valor final 0 , quiere decir que se completaron todas las celdas"""
     if contador == 0:
         return True
+
         """se llama al metodo winner para determinar si existio un ganador"""
     elif winner(board) is not None:
         return True
@@ -112,7 +120,8 @@ def terminal(board):
 
 
 def utility(board):
-    """si el ganador es X retorna 1, si el ganador es o retorna -1 y si existe un empate retorna 0"""
+    """si el ganador es X retorna 1, si el ganador es o retorna -1
+    y si existe un empate retorna 0"""
     if winner(board) == X:
         return 1
     elif winner(board) == O:
@@ -121,38 +130,40 @@ def utility(board):
         return 0
 
 
-
 def minimax(board):
     jugador = player(board)
 
     if jugador == X:
+        """se declara una variable constante"""
         v = -math.inf
         for movimiento in actions(board):
-            k = min_value(result(board, movimiento))  # FIXED
+            k = min_value(result(board, movimiento))
             if k > v:
                 v = k
                 best_move = movimiento
     else:
         v = math.inf
         for movimiento in actions(board):
-            k = max_value(result(board, movimiento))  # FIXED
+            k = max_value(result(board, movimiento))  
             if k < v:
                 v = k
                 best_move = movimiento
     return best_move
 
+
 def max_value(board):
     if terminal(board):
         return utility(board)
-    v = -math.inf
+    minimo_resultado = -math.inf
     for movimiento in actions(board):
-        v = max(v, min_value(result(board, movimiento)))
-    return v    #FIXED
+        minimo_resultado = max(minimo_resultado, min_value(result(board, movimiento)))
+    return minimo_resultado  # FIXED
+
 
 def min_value(board):
     if terminal(board):
         return utility(board)
-    v = math.inf
+    maximo_resultado = math.inf
     for movimiento in actions(board):
-        v = min(v, max_value(result(board, movimiento)))
-    return v    #FIXED
+        maximo_resultado = min(maximo_resultado, max_value(result(board, movimiento)))
+    return maximo_resultado  # FIXED
